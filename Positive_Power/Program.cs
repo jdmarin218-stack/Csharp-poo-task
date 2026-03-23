741f02b29a1b82fab25df121f42a0e429756af93
jdmarin218-stack <jdmarin218@gmail.com>
2026-03-23 10:32:40 -0500

    Se corrige Program.cs ejercicio Positive Power

--- Positive_Power/Program.cs
+++ Positive_Power/Program.cs
@@ -1,5 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace Positive_Power {
+    
 
+    class Program {
+        static void Main(string[] args) {
+        
+            // Ejecuta el ejercicio que desees probando cada método
+            Exercise1();
+        }
 
+        // 1. Positive Power
+        static void Exercise1()
+        {
+            Console.Write("Ingresa un número: ");
+            int n = int.Parse(Console.ReadLine());
+            if (n > 0)       Console.WriteLine($"Resultado: {n * n}");
+            else if (n == 0) Console.WriteLine("Resultado: 0");
+            else             Console.WriteLine("Resultado: Número negativo.");
+        }
+    }
+}
